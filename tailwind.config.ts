2a8370ad1fd323bc6333dec156d894f9c89f6bab
marthawerkflow <marthawerkflow@gmail.com>
2025-03-26 22:19:15 +0100

    Update tailwind.config.ts 🔒 Safelist bg-ivory voor Tailwind build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
 	darkMode: ["class"],
+	safelist: ["bg-ivory"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
