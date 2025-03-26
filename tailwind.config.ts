3ca45e8d3a617c8d97f81b28a5d700a9c12e5fdb
marthawerkflow <marthawerkflow@gmail.com>
2025-03-26 22:28:23 +0100

    Update tailwind.config.ts Added custom ivory color to Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,8 @@ export default {
 				ring: 'hsl(var(--ring))',
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
+				ivory: '#FFFFF0', // 
+				
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
 					foreground: 'hsl(var(--primary-foreground))'
